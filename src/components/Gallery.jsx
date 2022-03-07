ddee5742a41d3b2a65157e2064e4d0e516e97eec
Alexander Celis <alexcelis019@gmail.com>
2022-03-07 12:54:55 -0500

    add: gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import allImages from '../data'
+import iconCart from '../images/icon-cart-white.svg'
 
 const Thumbnail = ({ imgUrl }) => {
   return (
@@ -11,8 +12,24 @@ const Thumbnail = ({ imgUrl }) => {
 
 const Gallery = () => {
   const [firstPhoto, setFirstPhoto] = useState(allImages[0].img)
+
+  const btnStyles = {
+    background: 'tomato',
+    color: 'white',
+    padding: '1rem',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '1rem'
+  }
+
   return (
     <section className='container'>
+      <button style={btnStyles}>
+        <span>
+          <img src={iconCart} alt='' />
+        </span>
+        Comprar
+      </button>
       <main className='gallery'>
         <figure className='main-photo'>
           <img className='img-fluid' src={firstPhoto} alt='photo test' />
